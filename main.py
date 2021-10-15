93d641915402555822c0ba44370f62ea58e32817
AYUSH SINGH <76586285+CoderCatA5@users.noreply.github.com>
2021-10-15 15:08:39 +0530

    Fixed issue blue food
    fixed issue blue food
    added comments for better readablity

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ pygame.init()
 width,height=800,600#screen
 disp=pygame.display.set_mode((width,height))
 pygame.display.set_caption("SNEK")
-green,red,black,white=(0,204,153),(255,8,0),(0,0,0),(255,255,255)
+green,red,black,white,blue=(0,204,153),(255,8,0),(0,0,0),(255,255,255),(0,0,255)
 font_style=pygame.font.SysFont(None,30)
+
+
+
 
 def gameloop():
     end=0
+    godmode=0    #basically the variable that decides whether or not ure invincible
+
+    
     x,y,x1,y1=width/2,height/2,0,0#x,y->head pos;x1,y1->change in pos
     cell=20
     snake_speed=10
     body,blen=[],1
     clk=pygame.time.Clock()
 
-    #yeah all 
+    #ill be staying here to annoy u g
     red_counter=0
 
     food_x=round(random.randrange(0,width-cell)/cell)*cell
@@ -27,11 +33,16 @@ def gameloop():
 
     food_x_white=round(random.randrange(0,width-cell)/cell)*cell
     food_y_white=round(random.randrange(0,height-cell)/cell)*cell
-
     white_status=1
 
 
-    while not end:
+    food_x_blue=round(random.randrange(0,width-cell)/cell)*cell
+    food_y_blue=round(random.randrange(0,height-cell)/cell)*cell
+    blue_status=1
+    static_time=time.time()
+
+    while not end or godmode:
+        current_time=time.time()
         score_counter=blen-1
         #checks for keyboard inputs....
         for event in pygame.event.get():
@@ -56,9 +67,13 @@ def gameloop():
 
         #displays white box only if white status is 1
         if white_status:
+            
             pygame.draw.rect(disp,white,[food_x_white,food_y_white,cell,cell])
 
-
+        if blue_status:
+                        
+            pygame.draw.rect(disp,blue,[food_x_blue,food_y_blue,cell,cell])
+        
         head=[]
         head.append(x);head.append(y)
         body.append(head)#append new head to body
@@ -69,7 +84,6 @@ def gameloop():
             del body[0]
         for block in body:
             pygame.draw.rect(disp,green,[block[0],block[1],cell,cell])
-
         score=font_style.render("Score: "+str(blen-1),True,white)
         disp.blit(score,[0,0])
         pygame.display.update()
@@ -81,12 +95,16 @@ def gameloop():
             red_counter+=1
 
             if red_counter%2==0:
-                white_status=0 #white food dissapears when 2
+                white_status=0
+                blue_status=0 #white food dissapears when 2
 
             if red_counter%5==0:
                 white_status=1
+                blue_status=1
                 food_x_white=round(random.randrange(0,width-cell)/cell)*cell
                 food_y_white=round(random.randrange(0,height-cell)/cell)*cell
+                food_x_blue=round(random.randrange(0,width-cell)/cell)*cell
+                food_y_blue=round(random.randrange(0,height-cell)/cell)*cell
             if snake_speed<30: snake_speed+=0.5;
         
 
@@ -97,9 +115,24 @@ def gameloop():
 
             if score_counter>=3:score_counter-=3#reduces score by 3
             if snake_speed<30: snake_speed-=0.5;#slows the snake down
+        
+        elif food_x_blue==x and food_y_blue==y:
+            #contact with food blue
+            blue_status=0
+            static_time=time.time()
+            print(static_time)
+
+            #sets godmode to 1 invincible
+            godmode=1
+            if snake_speed<30: snake_speed+=1;#speeds up the snake 
+
+        #waits for 5 secs turns godmode off
+        if(current_time-static_time==5):godmode=0
 
         clk.tick(snake_speed)#fps
+    
 
+    
     clk.tick(snake_speed)
 
     disp.fill(black)
