d9b15e24f1d84189108a2955796e430445f1706b
picolloo <lucaspicolloo@gmail.com>
2021-10-15 10:57:11 -0300

    avoid food to be created below the snake

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ disp=pygame.display.set_mode((width,height))
 pygame.display.set_caption("SNEK")
 green,red,black,white=(0,204,153),(255,8,0),(0,0,0),(255,255,255)
 font_style=pygame.font.SysFont(None,30)
+cell=20
+
+def get_food_position(width, height, body):
+    while True:
+        food_x=round(random.randrange(0,width-cell)/cell)*cell
+        food_y=round(random.randrange(0,height-cell)/cell)*cell
+
+        if [food_x, food_y] not in body:
+            return food_x, food_y
+
 def gameloop():
     end=0
     x,y,x1,y1=width/2,height/2,0,0#x,y->head pos;x1,y1->change in pos
-    cell=20
     snake_speed=10
     body,blen=[],1
     clk=pygame.time.Clock()
-    food_x=round(random.randrange(0,width-cell)/cell)*cell
-    food_y=round(random.randrange(0,height-cell)/cell)*cell
+    food_x, food_y= get_food_position(width,height, body)
     while not end:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -48,10 +56,10 @@ def gameloop():
         disp.blit(score,[0,0])
         pygame.display.update()
         if food_x==x and food_y==y:#contact with food
-            food_x=round(random.randrange(0,width-cell)/cell)*cell
-            food_y=round(random.randrange(0,height-cell)/cell)*cell
+            food_x, food_y= get_food_position(width,height, body)
             blen+=1#body length increases
-            if snake_speed<30: snake_speed+=0.5;
+            if snake_speed<30:
+                snake_speed+=0.5
         clk.tick(snake_speed)#fps
     clk.tick(snake_speed)
     disp.fill(black)
@@ -64,3 +72,4 @@ def gameloop():
     pygame.quit()
     quit()
 gameloop()
+
