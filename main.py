e80eb9d1619070150811f4a159a85df12a496096
squirrellovespie <71578210+squirrellovespie@users.noreply.github.com>
2021-10-16 10:20:32 +0530

    original

--- main.py
+++ main.py
@@ -2,32 +2,20 @@ import pygame
 import time
 import random
 pygame.init()
-pygame.mixer.init()
-EAT_SOUND = pygame.mixer.Sound('./add.mp3')
-END_SOUND = pygame.mixer.Sound('./end.mp3')
 width,height=800,600#screen
 disp=pygame.display.set_mode((width,height))
 pygame.display.set_caption("SNEK")
-green,red,black,white,brown=(0,204,153),(255,8,0),(0,0,0),(255,255,255),(165,42,42)
+green,red,black,white=(0,204,153),(255,8,0),(0,0,0),(255,255,255)
 font_style=pygame.font.SysFont(None,30)
-cell=20
-
-def get_food_position(width, height, body):
-    while True:
-        food_x=round(random.randrange(0,width-cell)/cell)*cell
-        food_y=round(random.randrange(0,height-cell)/cell)*cell
-
-        if [food_x, food_y] not in body:
-            return food_x, food_y
-
 def gameloop():
     end=0
     x,y,x1,y1=width/2,height/2,0,0#x,y->head pos;x1,y1->change in pos
+    cell=20
     snake_speed=10
-    level = 1
     body,blen=[],1
     clk=pygame.time.Clock()
-    food_x, food_y= get_food_position(width,height, body)
+    food_x=round(random.randrange(0,width-cell)/cell)*cell
+    food_y=round(random.randrange(0,height-cell)/cell)*cell
     while not end:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -59,38 +47,18 @@ def gameloop():
         score=font_style.render("Score: "+str(blen-1),True,white)
         disp.blit(score,[0,0])
         pygame.display.update()
-        level_display = font_style.render("Level: " + str(level), True, white)
-        disp.blit(level_display,[(width - 80),0])
-        pygame.display.update()
-        speed_display = font_style.render("Speed: " + str(snake_speed), True, white)
-        disp.blit(speed_display,[10,height-20])
-        pygame.display.update()
         if food_x==x and food_y==y:#contact with food
-            food_x, food_y= get_food_position(width,height, body)
+            food_x=round(random.randrange(0,width-cell)/cell)*cell
+            food_y=round(random.randrange(0,height-cell)/cell)*cell
             blen+=1#body length increases
-
-            EAT_SOUND.play()
             if snake_speed<30: snake_speed+=0.5;
-            if(blen % 10 == 1):
-                level += 1
-
         clk.tick(snake_speed)#fps
     clk.tick(snake_speed)
     disp.fill(black)
     m=font_style.render("Game Over",True,red)
-    END_SOUND.play()
     disp.blit(m,[(width/2)-40,height/2])
-    f = open("score.txt","a")
-    f.write(str(blen-1)+"\n")
-    f.close()
-    with open("score.txt", "r") as f:
-        score = f.read() # Read all file in case values are not on a single line
-        score_ints = [ int(x) for x in score.split() ] # Convert strings to ints
-    highscore = max(score_ints) # sum all elements of the list
     f_score=font_style.render("Score: "+str(blen-1),True,white)
     disp.blit(f_score,[(width/2)-30,(height/2)+27])
-    f_hscore=font_style.render("High Score: "+str(highscore),True,white)
-    disp.blit(f_hscore,[(width/2)-50,(height/2)+50])
     pygame.display.update()
     time.sleep(2)
     pygame.quit()
